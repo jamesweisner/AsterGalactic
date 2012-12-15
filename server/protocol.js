fd4fe92d2664e3a34e5b7a42b580969bb6dbdd61
jamesweisner <james.weisner@gmail.com>
2012-12-14 19:06:35 -0800

    Begin work on object oriented persistent data storage.

--- server/protocol.js
+++ server/protocol.js
@@ -2,17 +2,46 @@ exports.handler = function(socket)
 {
 	socket.on('disconnect', function()
 	{
-		
+		// TODO send player goes offline to others?
 	});
-	socket.on('login', function(sessionId)
+	socket.on('loginRequest', function(username, password)
 	{
-		socket.on('loadGalaxy', function()
+		var message = 'success';
+		var player = new Player(); // https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/create
+		if(!(username in AsterGalactic.players))
+			message = 'Player does not exist';
+		else if(AsterGalactic.players[username].password != password)
+			message = 'Password incorrect.';
+		else
 		{
-			
+			player = AsterGalactic.players[username];
+			// TODO what if the player is already online?
+			// TODO whould we inform others that this player has come online?
+			socket.player = player;
+		}
+		socket.emit('loginResponse', {
+			'message': message,
+			'player': player.loginResponse()
+		});
+		
+		socket.on('loadGalaxy', function(sequence, cache_time)
+		{
+			var server_time = +new Date;
+			socket.emit('updateGalaxy', AsterGalactic.galaxy.updateGalaxy());
+			for(var i = 0; i < AsterGalactic.sectors.length; i++)
+			{
+				var sector = AsterGalactic.sectors[i];
+				if(sector.time < cache_time) continue; // Cache is still good.
+				socket.emit('updateSector', {
+					"sequence": sequence,
+					"time": server_time,
+					"sector": sector.changes_since(cache_time) // TODO implement sector.changes_since
+				});
+			}
 		});
 		socket.on('loadSector', function(sectorId)
 		{
-			
+			var server_time = +new Date;
 		});
 		socket.on('loadSystem', function(systemId)
 		{
@@ -59,4 +88,4 @@ exports.handler = function(socket)
 			
 		});
 	});
-});
+};
